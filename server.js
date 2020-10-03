9edd2724aa007477793b96ef634a8587e4d8b5e7
gtankha <46304828+gtankha@users.noreply.github.com>
2020-10-03 12:06:41 -0700

    Heroku Port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // ===========================================================
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 // parse incoming string or array data
