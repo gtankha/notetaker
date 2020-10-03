784affcf787490ea07b30b24b7ecfb278a702d51
gtankha <46304828+gtankha@users.noreply.github.com>
2020-10-03 16:31:29 -0700

    Fixed delete issues

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
-const { createNote, deleteNote } = require('../../lib/manageNotes');
+const { createNote, deleteNote, readNote } = require('../../lib/manageNotes');
+const fs = require('fs');
+const path = require('path');
 
 
 router.get('/notes', (req, res) => {
-    if (notes) {
-        console.log ("noteswali:  "+ notes);
-        res.json(notes);
+
+    const newNotes = readNote();
+    if (newNotes) {
+    const newNotesObj = JSON.parse(newNotes);
+    res.json(newNotesObj.notes);
     }
     else {
         res.status(400).send('The note is not properly formatted.');
@@ -15,8 +19,10 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     // if any data in req.body is incorrect, send 400 error back
+    const newNotes = readNote();
+    const newNotesObj = JSON.parse(newNotes);
     if (req.body.title && req.body.text) {
-        const results = createNote(req.body, notes);
+        const results = createNote(req.body, newNotesObj.notes);
         res.json(results);
     } else {
         res.status(400).send('The note is not properly formatted.');
@@ -25,8 +31,10 @@ router.post('/notes', (req, res) => {
 
 router.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
-    console.log("res  before " + notes);
-    const results = deleteNote(id, notes);
+    const newNotes = readNote();
+    const newNotesObj = JSON.parse(newNotes);
+    console.log("res  before " + newNotes);
+    const results = deleteNote(id, newNotesObj.notes);
     console.log("res  after " + results);
     res.json(results);
 });
